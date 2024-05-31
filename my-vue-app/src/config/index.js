2a8e7dc003235ad8b491e01d6cb5f0eab1088c4f
user1 <user1@lab3.com>
2024-06-01 00:06:29 +0800

    first commit

---
+++ my-vue-app/src/config/index.js
@@ -0,0 +1,32 @@
+/**
+ * 环境配置文件
+ * 一般在企业级项目里面有三个环境
+ * 开发环境
+ * 测试环境
+ * 线上环境
+ */
+// 当前的环境
+const env = import.meta.env.MODE || 'prod'
+
+const EnvConfig = {
+  development: {
+    baseApi: '/api',
+    mockApi: 'https://mock.apifox.com/m1/4068509-0-default/api/',
+  },
+  test: {
+    baseApi: '//test.future.com/api',
+    mockApi: 'https://mock.apifox.com/m1/4068509-0-default/api/',
+  },
+  pro: {
+    baseApi: '//future.com/api',
+    mockApi: 'https://mock.apifox.com/m1/4068509-0-default/api/',
+  },
+}
+
+export default {
+  env,
+  // mock的总开关,
+  mock: false,
+  ...EnvConfig[env]
+}
+
